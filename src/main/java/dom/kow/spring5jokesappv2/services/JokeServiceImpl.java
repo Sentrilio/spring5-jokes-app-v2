b1d2b124beb10fa7fd61ee71336d10bdfaa64852
Sentrilio <domkow@protonmail.com>
2021-03-20 13:19:37 +0100

    Complete Joke App exercise

---
+++ src/main/java/dom/kow/spring5jokesappv2/services/JokeServiceImpl.java
@@ -0,0 +1,14 @@
+package dom.kow.spring5jokesappv2.services;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JokeServiceImpl implements JokeService {
+
+
+    public String getJoke() {
+        ChuckNorrisQuotes chuckNorrisQuotes = new ChuckNorrisQuotes();
+        return chuckNorrisQuotes.getRandomQuote();
+    }
+}
